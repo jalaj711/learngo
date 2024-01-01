288e188223e537e96e84689aa7f4908a4afdfdaf
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-01-01 11:54:14 +0530

    added 0049-group-anagrams.go
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ leetcode/0049-group-anagrams.go
@@ -0,0 +1,47 @@
+package main
+
+import "sort"
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
+
+func groupAnagrams(strs []string) [][]string {
+	anagrams := make(map[string][]string)
+	for _, str := range strs {
+		sorted := SortString(str)
+		val, ok := anagrams[sorted]
+		if ok {
+			val = append(val, str)
+			anagrams[sorted] = val
+		} else {
+			anagrams[sorted] = []string{str}
+		}
+
+	}
+	ans := make([][]string, 0, len(anagrams))
+	for _, v := range anagrams {
+		ans = append(ans, v)
+	}
+	return ans
+}
+
+func main() {
+	groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
+}
