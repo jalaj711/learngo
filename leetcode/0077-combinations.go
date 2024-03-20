a61c59947959e9611514f59796097a4130fe1d65
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-03-20 19:00:24 +0530

    added 0077-combinations.go
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ leetcode/0077-combinations.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func _recur(curr []int, ind int, ll int, ul int, ans *[][]int) {
+	if ind >= len(curr) || ll > ul {
+		return
+	}
+	if ll == ul {
+		tmp := make([]int, len(curr))
+		copy(tmp, curr)
+		tmp[ind] = ll
+		*ans = append(*ans, tmp)
+		return
+	}
+
+	for i := ll; i <= ul-len(curr)+ind+1; i++ {
+		curr[ind] = i
+		if ind == len(curr)-1 {
+			tmp := make([]int, len(curr))
+			copy(tmp, curr)
+			*ans = append(*ans, tmp)
+		} else {
+			_recur(curr, ind+1, i+1, ul, ans)
+		}
+	}
+}
+
+func combine(n int, k int) [][]int {
+	ans := make([][]int, 0, 1)
+	curr := make([]int, k)
+	_recur(curr, 0, 1, n, &ans)
+	return ans
+}
+
+func main() {
+	fmt.Println(combine(5, 3))
+}
